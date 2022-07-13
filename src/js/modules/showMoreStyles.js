be7763d9c5edae98b07b44291450e1dd94714ef3
Vika <viktoriav.aero@gmail.com>
2022-07-13 20:37:33 +0300

    added response from server

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,19 +1,44 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import { getResource } from "../services/requests";
+
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
     
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp');
+    //------First way
+    // btn.addEventListener('click', function() {
+    //     getResource('http://localhost:3000/styles') --> get from localhost
+    //         .then(res => createCards(res))
+    //         .catch(error => console.log(error));
+        
+    //     this.remove();
+    // });
+
+    //-----Second
+     btn.addEventListener('click', function() {
+        getResource('assets/db.json') //-->link to file
+            .then(res => createCards(res.styles)) //--> path to arr styles
+            .catch(error => console.log(error));
+        
+       this.remove();
     });
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-        });
-        // btn.style.display = 'none';
-        btn.remove();
-    });
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1')
+        
+            card.innerHTML = `
+                <div class="styles-block">
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+        
+        })
+    }
 };
 
 export default showMoreStyles;
